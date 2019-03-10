acbca490c91f6fd9bb949dd46e117a01dd83f33c
Kevin Chan <kevin.chaan@gmail.com>
2019-03-10 16:17:21 -0700

    test keymap

--- keyboards/handwired/dactyl_manuform/6x6/keymaps/test/keymap.c
+++ keyboards/handwired/dactyl_manuform/6x6/keymaps/test/keymap.c
@@ -15,42 +15,42 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_QWERTY] = LAYOUT_6x6(
 
-     KC_F1  , KC_F2 , KC_F3 , KC_F4 , KC_F5 , KC_F6 ,                         KC_F7 , KC_F8 , KC_F9 ,KC_F10 ,KC_F11 ,KC_F12 ,
      KC_ESC , KC_1  , KC_2  , KC_3  , KC_4  , KC_5  ,                         KC_6  , KC_7  , KC_8  , KC_9  , KC_0  ,KC_BSPC,
-     KC_TAB , KC_Q  , KC_W  , KC_E  , KC_R  , KC_T  ,                         KC_Y  , KC_U  , KC_I  , KC_O  , KC_P  ,KC_MINS,
-     KC_LSFT, KC_A  , KC_S  , KC_D  , KC_F  , KC_G  ,                         KC_H  , KC_J  , KC_K  , KC_L  ,KC_SCLN,KC_QUOT,
-     KC_LCTL, KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,                         KC_N  , KC_M  ,KC_COMM,KC_DOT ,KC_SLSH,KC_BSLASH,
-                      KC_LBRC,KC_RBRC,                                                       KC_PLUS, KC_EQL,
-                                      RAISE,KC_SPC,                        KC_ENT, LOWER,
-                                      KC_TAB,KC_HOME,                         KC_END,  KC_DEL,
-                                      KC_BSPC, KC_GRV,                        KC_LGUI, KC_LALT
+     KC_TAB , KC_Q  , KC_W  , KC_E  , KC_R  , KC_T  ,                         KC_Y  , KC_U  , KC_I  , KC_O  , KC_P  ,KC_SLASH,
+     KC_CAPS, KC_A  , KC_S  , KC_D  , KC_F  , KC_G  ,                         KC_H  , KC_J  , KC_K  , KC_L  ,KC_SCLN,KC_ENT,
+     KC_LSFT, KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,                         KC_N  , KC_M  ,KC_COMM,KC_DOT , KC_UP ,KC_RSFT,
+     KC_GRAVE,KC_LCTL,KC_BSLASH,LGUI(KC_N),LGUI(KC_V),KC_DEL,                       KC_PLUS,KC_MINS,KC_COMM,KC_LEFT,KC_DOWN,KC_RIGHT,
+                      LGUI(KC_W),KC_LALT,                                                       KC_LALT, KC_EQL,
+                                      KC_SPC ,KC_LCMD,                        KC_LCMD,KC_ENT,
+                                      LGUI(KC_Z),SGUI(KC_V),                        KC_MINS,KC_PGUP,
+                                      LGUI(KC_S),LGUI(KC_W),                        KC_PLUS,KC_PGDOWN
   ),
 
   [_LOWER] = LAYOUT_6x6(
 
-     KC_F1 , KC_F2 , KC_F3 , KC_F4 , KC_F5 , KC_F6  ,                         KC_F7 , KC_F8 , KC_F9 ,KC_F10 ,KC_F11 ,KC_F12 ,
-     KC_TILD,KC_EXLM, KC_AT ,KC_HASH,KC_DLR ,KC_PERC,                        KC_CIRC,KC_AMPR,KC_ASTR,KC_LPRN,KC_RPRN,KC_DEL,
-     _______,_______,_______,_______,_______,KC_LBRC,                        KC_RBRC, KC_P7 , KC_P8 , KC_P9 ,_______,KC_PLUS,
-     _______,KC_HOME,KC_PGUP,KC_PGDN,KC_END ,KC_LPRN,                        KC_RPRN, KC_P4 , KC_P5 , KC_P6 ,KC_MINS,KC_PIPE,
-     _______,_______,_______,_______,_______,_______,                        _______, KC_P1 , KC_P2 , KC_P3 ,KC_EQL ,KC_UNDS,
-                                             _______,KC_PSCR,            _______, KC_P0,
+       _______, KC_F1 , KC_F2 , KC_F3 , KC_F4 , KC_F5 ,                        KC_F6  , KC_F7 , KC_F8 , KC_F9 ,KC_F10 ,KC_F11,
+       RESET  ,LGUI(KC_Q),LGUI(KC_W), KC_LCBR, KC_RCBR, _______,                        KC_AT  , KC_7  , KC_8  , KC_9  , KC_0  ,KC_PMNS,
+       _______,LGUI(KC_A),KC_PIPE,KC_LBRC,KC_RBRC,_______,                      KC_DLR , KC_4  , KC_5  , KC_6  ,KC_QUOT,KC_DQUO,
+       _______,LGUI(KC_Z),_______,KC_SLSH,KC_BSLS,_______,                      KC_PSLS, KC_1  , KC_2  , KC_3  ,KC_QUES,KC_PPLS,
+       _______,_______,_______,_______,_______,_______,                        _______,_______,KC_DOT , KC_0  , KC_EQL,_______,
                                              _______,_______,            _______,_______,
                                              _______,_______,            _______,_______,
-                                             _______,_______,            _______,_______
+                                             _______,_______,            _______,KC_HOME,
+                                             _______,_______,            _______,KC_END
   ),
 
   [_RAISE] = LAYOUT_6x6(
 
-       KC_F12 , KC_F1 , KC_F2 , KC_F3 , KC_F4 , KC_F5 ,                        KC_F6  , KC_F7 , KC_F8 , KC_F9 ,KC_F10 ,KC_F11 ,
-       _______,_______,_______,_______,_______,KC_LBRC,                        KC_RBRC,_______,KC_NLCK,KC_INS ,KC_SLCK,KC_MUTE,
-       _______,KC_LEFT,KC_UP  ,KC_DOWN,KC_RGHT,KC_LPRN,                        KC_RPRN,KC_MPRV,KC_MPLY,KC_MNXT,_______,KC_VOLU,
-       _______,_______,_______,_______,_______,_______,                        _______,_______,_______,_______,_______,KC_VOLD,
+       _______,_______,_______,_______,_______,_______,                        _______,_______,_______,_______,_______,RCTL(RSFT(KC_F)),
        _______,_______,_______,_______,_______,_______,                        _______,_______,_______,_______,_______,_______,
+       _______,_______,_______,_______,_______,_______,                        RCTL(RGUI(KC_H)),_______,_______,LALT(KC_QUOT),LGUI(RALT(KC_F)),_______,
+       _______,_______,_______,_______,_______,_______,                        LALT(LGUI(KC_M)),LGUI(KC_TAB),LALT(RGUI(KC_LEFT)),LALT(RGUI(KC_RGHT)),RGUI(KC_RBRC),_______,
+       _______,_______,_______,_______,_______,_______,                        _______,KC_HOME,KC_PGUP,_______,_______,KC_SLEP,
 
-                                               _______,_______,            KC_EQL ,_______,
-                                               _______,_______,            _______,_______,
-                                               _______,_______,            _______,_______,
-                                               _______,_______,            _______,_______
+                                               _______,_______,            KC_END ,KC_PGDOWN,
+                                               _______,_______,            _______,RCTL(RGUI(KC_A)),
+                                               _______,_______,            _______,KC_HOME,
+                                               _______,_______,            _______,KC_END
   ),
 
 };
