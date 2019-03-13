5cc233ab9a58d3c1b1f571a463519c82803fdac0
Kevin Chan <kevin.chaan@gmail.com>
2019-03-12 22:22:45 -0700

    adding level 1 & 2 toggles

--- keyboards/handwired/dactyl_manuform/6x6/keymaps/test/keymap.c
+++ keyboards/handwired/dactyl_manuform/6x6/keymaps/test/keymap.c
@@ -19,11 +19,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_TAB , KC_Q  , KC_W  , KC_E  , KC_R  , KC_T  ,                         KC_Y  , KC_U  , KC_I  , KC_O  , KC_P  ,KC_SLASH,
      KC_CAPS, KC_A  , KC_S  , KC_D  , KC_F  , KC_G  ,                         KC_H  , KC_J  , KC_K  , KC_L  ,KC_SCLN,KC_ENT,
      KC_LSFT, KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,                         KC_N  , KC_M  ,KC_COMM,KC_DOT , KC_UP ,KC_RSFT,
-     KC_GRAVE,KC_LCTL,KC_BSLASH,LGUI(KC_N),LGUI(KC_V),KC_DEL,                       KC_PLUS,KC_MINS,KC_COMM,KC_LEFT,KC_DOWN,KC_RIGHT,
+     LT(_RAISE,KC_GRAVE),KC_LCTL,KC_BSLASH,LGUI(KC_N),LGUI(KC_V),KC_DEL,                       LT(_RAISE,KC_PLUS),LT(_LOWER,KC_MINS),KC_COMM,KC_LEFT,KC_DOWN,KC_RIGHT,
                       LGUI(KC_W),KC_LALT,                                                       KC_LALT, KC_EQL,
                                       KC_SPC ,KC_LCMD,                        KC_LCMD,KC_ENT,
-                                      LGUI(KC_Z),SGUI(KC_V),                        KC_MINS,KC_PGUP,
-                                      LGUI(KC_S),LGUI(KC_W),                        KC_PLUS,KC_PGDOWN
+                                      LGUI(KC_Z),SGUI(KC_V),                        LT(_LOWER,KC_MINS),KC_PGUP,
+                                      LGUI(KC_S),LGUI(KC_W),                        LT(_RAISE,KC_PLUS),KC_PGDOWN
   ),
 
   [_LOWER] = LAYOUT_6x6(
